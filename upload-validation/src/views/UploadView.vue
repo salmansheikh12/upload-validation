794d9f69a8ac96c28bb7815c735050fd3b60c416
Ohwhenn <owenramirez0329@gmail.com>
2023-02-21 20:19:37 -0800

    isa button addition

---
+++ upload-validation/src/views/UploadView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="upload">
+    This is an upload page
+    <button>ISA BUTTON</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .upload {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
