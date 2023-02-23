fe953ff7367a2dff9fc624bf348797bdc06336e0
Salman Sheikh <SalmanSheikh2180@gmail.com>
2023-02-22 22:00:24 -0800

    navbar

--- upload-validation/src/App.vue
+++ upload-validation/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img
       alt="Vue logo"
       class="logo"
@@ -21,13 +21,50 @@ import HelloWorld from "./components/HelloWorld.vue";
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
 
-  <RouterView />
+  <!-- <RouterView /> -->
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+    <a class="navbar-brand" href="#">Your Moon</a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="nav-link"><RouterLink to="/">Home</RouterLink></a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link"><RouterLink to="/about">About</RouterLink></a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link"><RouterLink to="/upload">Upload Images</RouterLink></a>
+        </li>
+
+      </ul>
+
+
+
+    </div>
+  </nav>
+  
 </template>
 
 <style scoped>
-header {
+
+
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -87,5 +124,43 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
+} */
+
+
+nav{
+
+display: flex;
+align-items: center;
+justify-content: space-between;
+padding-top: 5px;
+padding-left: 3%; 
+padding-right: 3%;
+background-color: transparent;
+
+position: relative;
+z-index: 7;
+
+
+font-size: 14px;
+
+
 }
+
+nav ul li a {
+    color: white;
+    text-decoration: none;
+    font-weight: bold;
+    transition: .3s;
+}
+
+nav ul li a:hover {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+ 
+
+}
+
+
 </style>
+
+
